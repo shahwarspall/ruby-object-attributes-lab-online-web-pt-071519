4b5f49e03f974570f43f16c5fdbe99b0a4521fb3
Shahwar Spall <shevy.spall@gmail.com>
2019-07-31 03:08:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
-  def initialize(dog_names)
+  def name(dog_names)
     @name = dog_names 
   end
   def name
     @name 
   end
   
-  def initialize(dog_breeds)
+  def breed(dog_breeds)
     @breed = dog_breeds
   end
 end
