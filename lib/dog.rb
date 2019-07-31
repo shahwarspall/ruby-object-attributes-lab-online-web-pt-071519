a68ea0896445648e71e44a493490827e01f6f620
Shahwar Spall <shevy.spall@gmail.com>
2019-07-31 03:55:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def breed=(dog_breeds)
+    @breed = dog_breeds
+  end
+end
